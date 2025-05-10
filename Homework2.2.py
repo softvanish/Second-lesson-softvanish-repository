9240ad98d4fe5270a6ffe21576947b0027509ac2
softvanish <softvanish@gmail.com>
2025-05-10 14:19:50 +0300

    new change

---
+++ Homework2.2.py
@@ -0,0 +1,9 @@
+
+user_num = int(input("Please enter your four digit number: "))
+
+num1, num2 = divmod(user_num, 10000)
+num2, num3 = divmod(num2, 1000)
+num3, num4 = divmod(num3, 100)
+num4, num5 = divmod(num4, 10)
+
+print(num5, num4, num3, num2, num1, sep= "")
