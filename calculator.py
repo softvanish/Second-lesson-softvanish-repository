edfa435e7bc325fb2a8eb5dbd9e497ae3bc34f9e
softvanish <softvanish@gmail.com>
2025-05-14 14:22:33 +0300

    new change

---
+++ calculator.py
@@ -0,0 +1,56 @@
+# Пытаюсь сам сделать калькулятор
+
+###
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def substract(self, a, b):
+        return a - b
+
+
+    def multiply(self, a, b):
+        return a * b
+
+
+    def divide(self, a, b):
+        return a / b
+
+calc = Calculator()
+
+###
+
+print("It's my calculator, let's test it!")
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+
+operation = input("Choose your option \"+, -, *, /\": ")
+
+###
+
+if operation == "+":
+    calc.add(num1, num2)
+    result = calc.add(num1, num2)
+
+elif operation == "-":
+    calc.substract(num1, num2)
+    result = calc.substract(num1, num2)
+
+elif operation == "*":
+    calc.multiply(num1, num2)
+    result = calc.multiply(num1, num2)
+
+elif operation == "/":
+    calc.divide(num1, num2)
+    result = calc.divide(num1, num2)
+
+else:
+    print("Wrong")
+    result = None
+
+###
+
+if result is not None:
+    print(f"Your result: {result} ")
