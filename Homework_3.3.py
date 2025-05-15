97f978db029c7c0c88e0197011638271f5b90d3f
softvanish <softvanish@gmail.com>
2025-05-15 21:07:00 +0300

    new change

---
+++ Homework_3.3.py
@@ -0,0 +1,56 @@
+#Розділити один список на два списки
+
+
+
+#Exercise №1
+
+lst = [1, 2, 3, 4, 5, 6]
+
+# ex1 = lst[:3]
+# ex2 = lst[3::]
+#
+# lst2 = []
+# lst2.append(ex1)
+# lst2.append(ex2)
+# print(lst2)
+
+
+
+#Exercise №2
+
+second_lst = [1, 2, 3]
+
+# ex1 = second_lst[0:2]
+# ex2 = second_lst[2:]
+#
+# second_lst2 = []
+# second_lst2.append(ex1)
+# second_lst2.append(ex2)
+# print(second_lst2)
+
+
+
+#Exercise №3
+
+third_lst = [1, 2, 3, 4, 5]
+
+# ex1 = third_lst[:3]
+# ex2 = third_lst[3:]
+#
+# third_lst2 = []
+# third_lst2.append(ex1)
+# third_lst2.append(ex2)
+# print(third_lst2)
+
+
+
+#Exercise №4
+
+fourth_lst = []
+
+ex1 = []
+ex2 = []
+
+fourth_lst.append(ex1)
+fourth_lst.append(ex2)
+print(fourth_lst)
