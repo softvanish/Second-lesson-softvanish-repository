fe2289ba96cde546db76c30d87f2e9093bcf3ec4
softvanish <softvanish@gmail.com>
2025-05-17 15:09:55 +0300

    new change

--- JustTest.py
+++ JustTest.py
@@ -1,15 +1,76 @@
-'''
-Тестирую конструкцию def
-'''
+# '''
+# Тестирую конструкцию def
+# '''
+#
+# print("We will multiply your numbers!")
+#
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+#
+# def multiply(num1, num2):
+#     return num1 * num2
+#
+# result = multiply(num1, num2)
+# print(f"Your answer is: {result}")
 
-print("We will multiply your numbers!")
+###
 
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+#
+# n = 1
+# while n <= 10:
+#     print(n)
+#     n = n+1
+# print("End")
 
-def multiply(num1, num2):
-    return num1 * num2
+###
 
-result = multiply(num1, num2)
-print(f"Your answer is: {result}")
+# n = 0
+#
+# while n < 10:
+#     n = n + 1
+#     if n == 5:
+#         continue
+#     print(n)
+
+###
+
+# number = 0
+#
+# while number <= 10:
+#     number = number + 1
+#     if number == 5:
+# 				# Цикл завершить свою роботу тоді, коли значення number дорівнюватиме 5
+#         break
+#     print(number)
+# print('End')
+
+###
+
+
+# number = 0
+# while True:
+#     number = number + 1
+#     if number == 5:
+#         continue
+#     if number == 11:
+#         break
+#     print(number)
+
+###
+
+# # виведе на екран прямокутник із символів «*».
+# a = int(input("Input a "))
+# b = int(input("Input b "))
+# i = 0
+# while i < a: # Висота
+#     j = 0
+#     while j < b: # ширина
+#         print("*", end='') # рядок не буде переведено
+#         j += 1
+#     print()
+#     i += 1
+
+###
+
+
 
