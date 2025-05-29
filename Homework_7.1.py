d496341cc534ca90ff3ee3a7ba54a71955339c1a
softvanish <softvanish@gmail.com>
2025-05-29 17:34:59 +0300

    new homework

---
+++ Homework_7.1.py
@@ -0,0 +1,35 @@
+# Вітання
+
+
+
+# First example
+
+
+
+# def say_hi(name, age):
+#     print(f"Hello. My name is {name} and I'm {age} years old!")
+#
+# user_name = input("Enter your name please: ")
+# user_age = input("Enter your age please: ")
+#
+# say_hi(user_name, user_age)
+
+
+
+# Second example.
+
+
+
+def say_bye(name2, age2):
+
+    return (f"Bye {name2}! I believe your {age2}'s birthday was happy!")
+
+assert say_bye("John", 27) == "Bye John! I believe your 27's birthday was happy!"
+
+print("Yep")
+
+
+
+
+
+
