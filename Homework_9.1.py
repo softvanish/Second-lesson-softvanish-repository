bbb76dbdcdf356a42a71b335ba216a659ce0a445
softvanish <softvanish@gmail.com>
2025-06-05 17:10:36 +0300

    new homework

---
+++ Homework_9.1.py
@@ -0,0 +1,20 @@
+# Визначити популярність певних слів у тексті
+
+def popular_words (text, words):
+
+    text = text.lower()
+    text = text.split()
+
+    result = {word: 0 for word in words}
+
+    for word in words:
+
+        result[word] = text.count(word)
+
+    return result
+
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
+print('OK')
+
+
+
