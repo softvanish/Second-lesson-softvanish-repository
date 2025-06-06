50b757f054937a0b76769e6b18931a95d658ebdc
softvanish <softvanish@gmail.com>
2025-06-06 13:36:08 +0300

    new homework

--- JustTest.py
+++ JustTest.py
@@ -1,76 +1,21 @@
-# '''
-# Тестирую конструкцию def
-# '''
-#
-# print("We will multiply your numbers!")
-#
-# num1 = int(input("Enter the first number: "))
-# num2 = int(input("Enter the second number: "))
-#
-# def multiply(num1, num2):
-#     return num1 * num2
-#
-# result = multiply(num1, num2)
-# print(f"Your answer is: {result}")
+def difference(*args):
 
-###
+    if (args):
+        min_num = min(*args)
+        max_num = max(*args)
+        result = max_num - min_num
 
-#
-# n = 1
-# while n <= 10:
-#     print(n)
-#     n = n+1
-# print("End")
+        return result
 
-###
-
-# n = 0
-#
-# while n < 10:
-#     n = n + 1
-#     if n == 5:
-#         continue
-#     print(n)
-
-###
-
-# number = 0
-#
-# while number <= 10:
-#     number = number + 1
-#     if number == 5:
-# 				# Цикл завершить свою роботу тоді, коли значення number дорівнюватиме 5
-#         break
-#     print(number)
-# print('End')
-
-###
+    else:
+        return 0
 
 
-# number = 0
-# while True:
-#     number = number + 1
-#     if number == 5:
-#         continue
-#     if number == 11:
-#         break
-#     print(number)
 
-###
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+print('OK')
 
-# # виведе на екран прямокутник із символів «*».
-# a = int(input("Input a "))
-# b = int(input("Input b "))
-# i = 0
-# while i < a: # Висота
-#     j = 0
-#     while j < b: # ширина
-#         print("*", end='') # рядок не буде переведено
-#         j += 1
-#     print()
-#     i += 1
-
-###
-
-
+# assert difference(1, 2, 3) == 2, 'Test1'
+# assert difference(5, -5) == 10, 'Test2'
+# assert difference() == 0, 'Test4'
 
