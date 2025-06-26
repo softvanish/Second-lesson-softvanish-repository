fb1519038328ec2dedb06a75f8516613b0387626
softvanish <softvanish@gmail.com>
2025-06-26 20:31:04 +0300

    some other things

---
+++ Homework_15.2.py
@@ -0,0 +1,79 @@
+# Клас «Правильний дріб»
+class Fraction:
+    def __init__(self, a, b):
+        if b == 0:
+            raise ValueError("Denominator cannot be zero.")
+        self.a = a
+        self.b = b
+        #self.__reduce()
+
+    def __reduce(self):
+        def gcd(x, y):
+            while y:
+                x, y = y, x % y
+            return x
+
+        sign = -1 if (self.a > 0 > self.b) or \
+                     (self.a < 0 < self.b) else 1
+        a_abs, b_abs = abs(self.a), abs(self.b)
+        g = gcd(a_abs, b_abs)
+        self.a = sign * (a_abs // g)
+        self.b = b_abs // g
+
+    def __clone(self):
+        return Fraction(self.a, self.b)
+
+    def __mul__(self, other):
+        if isinstance(other, Fraction):
+            return Fraction(self.a * other.a, self.b * other.b)
+        raise ValueError("Can only multiply with another Fraction")
+
+    def __add__(self, other):
+        if isinstance(other, Fraction):
+            num = self.a * other.b + self.b * other.a
+            den = self.b * other.b
+            return Fraction(num, den)
+        raise ValueError("Can only add another Fraction")
+
+    def __sub__(self, other):
+        if isinstance(other, Fraction):
+            num = self.a * other.b - self.b * other.a
+            den = self.b * other.b
+            return Fraction(num, den)
+        raise ValueError("Can only subtract another Fraction")
+
+    def __eq__(self, other):
+        if isinstance(other, Fraction):
+            return self.a * other.b == other.a * self.b
+        return False
+
+    def __gt__(self, other):
+        if isinstance(other, Fraction):
+            return self.a * other.b > other.a * self.b
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, Fraction):
+            return self.a * other.b < other.a * self.b
+        return False
+
+    def __str__(self):
+        return f"Fraction: {self.a}, {self.b}"
+
+
+f_a = Fraction(2, 3)
+f_b = Fraction(3, 6)
+f_c = f_b + f_a
+assert str(f_c) == 'Fraction: 21, 18'
+f_d = f_b * f_a
+assert str(f_d) == 'Fraction: 6, 18'
+f_e = f_a - f_b
+assert str(f_e) == 'Fraction: 3, 18'
+
+assert f_d < f_c  # True
+assert f_d > f_e  # True
+assert f_a != f_b  # True
+f_1 = Fraction(2, 4)
+f_2 = Fraction(3, 6)
+assert f_1 == f_2  # True
+print('OK')
